ae7769d0d868814ef896f9cde5f6348099e3ed43
Eduardo Moura <edoura@gmail.com>
2015-10-28 16:21:55 -0200

    Melhorando o body.html, os itens da parte institucional do site

--- preCadastro.php
+++ preCadastro.php
@@ -8,4 +8,7 @@ if(isset($_POST['nome']) and isset($_POST['email'])){
 	cad->sendPost($_POST);
 }
 
+$sm->assign("titulo","Primeira etapa");
+$sm->assign("legend","Dados do Responsável");
+
 $sm->display("preCadastro.html");
